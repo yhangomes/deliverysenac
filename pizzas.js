277664720c1817c9874eac7d24d1cb13375bdbff
yhangomes <yhan.bgomes@senacsp.edu.br>
2025-06-27 11:27:29 -0300

    Correção de erro no Index

---
+++ pizzas.js
@@ -0,0 +1,38 @@
+let cardapio = [
+    {
+        foto: "imgs/Pizza1.jpg",
+        nome: "calabresa",
+        ingredientes: ["calabresa", "cebola", "muçarela"],
+        valor: 45.00
+    },
+    {
+        foto: "imgs/Pizza2.jpg",
+        nome: "frango com catupiry",
+        ingredientes: ["frango", "catupiry", "muçarela"],
+        valor: 50.00
+    },
+    {
+        foto: "imgs/Pizza3.jpg",
+        nome: "calanguinho",
+        ingredientes: ["calango", "cebola", "muçarela", "brocolis"],
+        valor: 60.00
+    },
+    {
+        foto: "imgs/Pizza4.jpg",
+        nome: "fuminho",
+        ingredientes: ["pimenta malagueta", "calabresa", "carne", "ralapenho", "camarao"],
+        valor: 80.00
+    },
+    {
+        foto: "imgs/Pizza5.jpg",
+        nome: "mussarela",
+        ingredientes: ["mussarela"],
+        valor: 45.00
+    },
+    {
+        foto: "imgs/Pizza6.jpg",
+        nome: "camarao com queijo",
+        ingredientes: ["mussarela", "camarao", "cebola"],
+        valor: 65.00
+    }
+];
